83b24b60d30be6870dc5341aea7b308e2b624054
evawythien <evadvgarcia@gmail.com>
2017-12-08 23:13:06 +0100

    Cambios

--- WebPorfolioGenerator/Controllers/PortfoliosController.cs
+++ WebPorfolioGenerator/Controllers/PortfoliosController.cs
@@ -49,6 +49,11 @@ namespace WebPorfolioGenerator.Controllers
             return View();
         }
 
+        public async Task<IActionResult> Preview()
+        {
+            return View(await _context.Portfolios.ToListAsync());
+        }
+
         // POST: Portfolios/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
