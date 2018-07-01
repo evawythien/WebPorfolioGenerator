ba002d95f5c6394c51fead422651037697bd586b
evawythien <evadvgarcia@gmail.com>
2018-07-01 19:15:13 +0200

    Cambios en la arquitectura.

---
+++ WebPortfolioGenerator.Domain/Entity/Rol.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebPortfolioGenerator.Domain.Entity
+{
+    [Table("Rols")]
+    public class Rol
+    {
+        [Key]
+        public int RolId { get; set; }
+
+        [StringLength(200)]
+        public string Name { get; set; }
+    }
+}
+
+
---
+++ WebPortfolioGenerator.Domain/Entity/Tag.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebPortfolioGenerator.Domain.Entity
+{
+    [Table("Tags")]
+    public class Tag
+    {
+        [Key]
+        public int TagId { get; set; }
+
+        public int PostId { get; set; }
+
+        [StringLength(200)]
+        public int Name { get; set; }
+    }
+}
