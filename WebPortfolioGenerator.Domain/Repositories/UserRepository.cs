20cfb17c6cbeea80b5849d663fa930c19da21517
evawythien <evadvgarcia@gmail.com>
2018-12-19 23:53:58 +0100

    Seguimos.

---
+++ WebPortfolioGenerator.Domain/Repositories/UserRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebPortfolioGenerator.Domain.Repositories
+{
+    public class UserRepository 
+    {
+    }
+}
---
+++ WebPortfolioGenerator.Infrastructure/Repositories/IPortfolioRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebPortfolioGenerator.Infrastructure.Repositories
+{
+    public interface IPortfolioRepository
+    {
+    }
+}
