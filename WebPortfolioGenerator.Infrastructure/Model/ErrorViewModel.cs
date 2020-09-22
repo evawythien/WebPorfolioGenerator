006187199fee1c495d7bde0725026a052bd36d05
Eva <evadvgarcia@gmail.com>
2020-09-22 23:31:40 +0200

    Revert "Cambios en la arquitectura"

--- WebPortfolioGenerator.Infrastructure/Model/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebPortfolioGenerator.Infrastructure.Model
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
